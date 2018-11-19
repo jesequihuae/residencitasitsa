3a76ec1dd0078cf89e66ae2a0cdefb1074b3d6b9
Maickoldsi12 <miguel.angel.rodriguez.cornejo@gmail.com>
2018-11-19 15:02:57 -0600

    update

--- pages/alertas/alertasPorAlumno.php
+++ pages/alertas/alertasPorAlumno.php
@@ -26,12 +26,12 @@
                      		</a>
 									 		</li>
 
-                     <li><a href="#doner" data-toggle="tab" title="completed">
+                    <!-- <li><a href="#doner" data-toggle="tab" title="completed">
                          <span class="round-tabs five">
                               <i class="glyphicon glyphicon-ok"></i>
                          </span> </a>
                      </li>
-
+-->
                      </ul></div>
 
                      <div class="tab-content">
@@ -120,7 +120,7 @@
 												</div>
                       </div>
 
-                      <div class="tab-pane fade" id="doner">
+                      <!--<div class="tab-pane fade" id="doner">
                           <h3 class="head text-center">Bootsnipp goodies</h3>
                           <p class="narrow text-center">
                               Lorem ipsum dolor sit amet, his ea mollis fabellas principes. Quo mazim facilis tincidunt ut, utinam saperet facilisi an vim.
@@ -128,7 +128,7 @@
                           <p class="text-center">
                     					<a href="" class="btn btn-success btn-outline-rounded green"> start using bootsnipp <span style="margin-left:10px;" class="glyphicon glyphicon-send"></span></a>
                 					</p>
-                      </div>
+                      </div>-->
 
 							<div class="clearfix"></div>
 							</div>
